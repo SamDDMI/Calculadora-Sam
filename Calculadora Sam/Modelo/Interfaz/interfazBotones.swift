bc41beb4a79831f0435145caa780b29b81701826
Sam <68865514+SamDDMI@users.noreply.github.com>
2024-09-09 10:10:49 -0600

    nuevos archivos de 9 de sep

---
+++ Calculadora Sam/Modelo/Interfaz/interfazBotones.swift
@@ -0,0 +1,81 @@
+//
+//  interfazBotones.swift
+//  Calculadora Sam
+//
+//  Created by alumno on 06/09/24.
+//
+
+import Foundation
+
+
+//interfaz usuario [nombre de la clase o estructura]
+struct IUBotonCalculadora{
+    var RESTORATIONid: String
+    var num: Character
+    var operacion: String
+    
+    init(_ id: String, numero: Character, operacion: String) {
+        self.RESTORATIONid = id
+        self.num = numero
+        self.operacion = operacion
+    }
+    
+
+    static func crear_arreglo_botones() -> Dictionary<String, IUBotonCalculadora> {
+        var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+        
+        botones_interfaz["boton_0"] = IUBotonCalculadora(
+            "boton_0",
+            numero: Character ("0"),
+            operacion: ""
+        )
+        botones_interfaz["boton_1"] = IUBotonCalculadora(
+            "boton_1",
+            numero: Character ("1"),
+            operacion: ""
+        )
+        botones_interfaz["boton_2"] = IUBotonCalculadora(
+            "boton_2",
+            numero: Character ("2"),
+            operacion: ""
+        )
+        botones_interfaz["boton_3"] = IUBotonCalculadora(
+            "boton_3",
+            numero: Character ("3"),
+            operacion: ""
+        )
+        botones_interfaz["boton_4"] = IUBotonCalculadora(
+            "boton_4",
+            numero: Character ("4"),
+            operacion: ""
+        )
+        botones_interfaz["boton_5"] = IUBotonCalculadora(
+            "boton_5",
+            numero: Character ("5"),
+            operacion: ""
+        )
+        botones_interfaz["boton_6"] = IUBotonCalculadora(
+            "boton_6",
+            numero: Character ("6"),
+            operacion: ""
+        )
+        botones_interfaz["boton_7"] = IUBotonCalculadora(
+            "boton_7",
+            numero: Character ("7"),
+            operacion: ""
+        )
+        botones_interfaz["boton_8"] = IUBotonCalculadora(
+            "boton_8",
+            numero: Character ("8"),
+            operacion: ""
+        )
+        botones_interfaz["boton_9"] = IUBotonCalculadora(
+            "boton_9",
+            numero: Character ("9s"),
+            operacion: ""
+        )
+        
+        return botones_interfaz
+    }
+    }
+
--- Calculadora Sam/ViewController.swift
+++ Calculadora Sam/ViewController.swift
@@ -11,17 +11,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var textito: UILabel!
     @IBOutlet weak var btn_interactuar: UIButton!
     @IBOutlet weak var BOTONES: UIStackView!
+    @IBOutlet weak var botonop: UIButton!
+    
+    var botones_interfaz: Array<IUBotonCalculadora> = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    inicializar_calculadora()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func esto_hace_el_boton(_ sender: UIButton) {
+        for boton in botones_interfaz{
+            if(boton.RESTORATIONid == sender.restorationIdentifier){
+                print(boton.num)
+                
+            }
+        }
+        //textito.text = sender.titleLabel!.text ?? "no supe"
+        //print(sender.restorationIdentifier)
         
-        textito.text = sender.titleLabel!.text ?? "no supe"
         
+        // hola mundo + boton1
     }
+    
+    func inicializar_calculadora() -> Void{
+        crear_arreglo_botones()
+    }
+    func crear_arreglo_botones() -> Void{
+        botones_interfaz = IUBotonCalculadora.crear_arreglo_botones()
+        
+        }
+    
     
 }
 
